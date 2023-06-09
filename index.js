4091d7f6ab02d5a72d39c209dfb9dc6bee336279
marcusholloway17 <loganjean17@gmail.com>
2023-06-09 20:24:30 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * @description handle queries object passed in the body or query and return it
+   * @param {object} req
+   * @returns {object}
+   */
+  handleRequest: (req) => {
+    const globalRequest = { ...req.body?._query, ...req.query?._query };
+    let query = {};
+    // in the body
+    //  handle attributes clause
+    query.attributes = globalRequest?.attributes || { exclude: [] };
+
+    // handle where clause
+    query.where = globalRequest?.where || [];
+
+    // handle order clause
+    query.order = globalRequest?.order || [];
+
+    // handle pagination
+    // handle pagination
+    if (globalRequest?.page && globalRequest?.pageSize) {
+      const page = parseInt(globalRequest?.page) || 1;
+      const pageSize = parseInt(globalRequest?.pageSize) || 10;
+
+      query.limit = pageSize;
+      query.offset = (page - 1) * pageSize;
+    }
+
+    // handle include
+    query.include = globalRequest?.include || [];
+
+    return query;
+  },
+};
